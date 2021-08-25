8931a84c68e25ae44991a5f6437dd2ddf771f62b
Jayson Helseth <jayson.helseth@gmail.com>
2021-08-25 09:20:21 -0500

    Added code to send data to the websocket client. This will be updated once the serial device is connected.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"log"
@@ -17,15 +18,28 @@ var (
 func readFromSerial() {
 	// Todo: Put sensor read loop in here
 	// Maybe move this code to a new file....
-	ws.WriteMessage(websocket.TextMessage, []byte("Sensor connected!"))
+	//ws.WriteMessage(websocket.TextMessage, []byte("Sensor connected!"))
+
+	var temp float32 = 0.0
+	for {
+		dict := map[string]float32{
+			"temp": temp,
+		}
+		msg, _ := json.Marshal(dict)
+		ws.WriteMessage(websocket.TextMessage, msg)
+
+		temp += 0.1
+		time.Sleep(time.Second * 1)
+	}
 }
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	// Todo: need cors accept code in here
-
 	upgrader := websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
 	}
 
 	var err error
@@ -38,16 +52,17 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ws.WriteMessage(websocket.TextMessage, []byte("I'm connected!"))
+	// ws.WriteMessage(websocket.TextMessage, []byte("I'm connected!"))
+	log.Println("I'm connected.")
 	readFromSerial()
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", serveWs)
+	router := mux.NewRouter()
+	router.HandleFunc("/", serveWs)
 
 	srv := &http.Server{
-		Handler: r,
+		Handler: router,
 		Addr:    "127.0.0.1:8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
